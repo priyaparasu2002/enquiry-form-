8e9ceed2e027dcc56d3346b74d9280d6df036829
priyaparasu2002 <160120335+priyaparasu2002@users.noreply.github.com>
2024-08-19 20:20:31 +0530

    Add files via upload

---
+++ 7enquiry form/script.js
@@ -0,0 +1,23 @@
+function sendEmail(){
+      Email.send({
+        Host:"smtp.gmail.com",
+        Username:"priyaparasuraman2002@gmail.com",
+        Password:"Priya2002*",
+        To:'admin2@gmail.com',
+        From:document.getElementById("email").value,
+        Subject:"New Contact Form Enquiry",
+        Body:"Name:"+document.getElementById("name").value
+        +"<br> Email:"+document.getElementById("email").value
+        +"<br> Mobile No:"+document.getElementById("mobile").value
+        +"<br> Message:"+document.getElementById("message").value
+    }).then(
+        message=>alert("message sent successfully")
+    );
+}
+
+
+
+
+
+
+    
